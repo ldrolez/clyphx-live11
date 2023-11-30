e6f61068fa018fa2bc9a527547ffe6e34ce75f7a
ldrolez <ldrolez@free.fr>
2023-11-30 13:57:22 +0100

    Version update

--- ClyphX/ClyphX.py
+++ ClyphX/ClyphX.py
@@ -50,7 +50,7 @@ if IS_LIVE_9_5:
     from .PushEmuHandler import MockHandshakeTask, MockHandshake
 
 FOLDER = '/ClyphX/'
-SCRIPT_NAME = 'ClyphX v2.7.3 for Live 11'
+SCRIPT_NAME = 'ClyphX v2.7.4 for Live 11'
 
 class ClyphX(ControlSurface):
     __module__ = __name__
